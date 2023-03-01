54a83f3ad8d835a884f8e71042ca8107958eae78
João Pereira <pereira.joao@gmail.com>
2023-03-01 23:11:02 +0000

    Start including queue Publisher and Subscriber.

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,92 @@
+﻿namespace FastCSharp.SDK.Publisher;
+
+using System.Text;
+using System.Text.Json;
+
+delegate T? Handler<T>(T? message);
+
+interface IPublisher<T>: IDisposable
+{ 
+    Task<Boolean> Publish(string? message);
+    Task<Boolean> Publish(T? message);
+
+    /// <summary>
+    /// Registers handler function to handle message before deelivering to the callback
+    /// Returns self to chain the addition of message handlers
+    /// </summary>
+    /// <param name="handler"></param>
+    /// <returns></returns>
+    public IPublisher<T> AddMsgHandler(Handler<T> handler);
+}
+
+interface IMessageQueueHandlerFactory<T>
+{
+    public IPublisher<T> NewPublisher();
+
+}
+
+abstract class AbstractPublisher<T> : IPublisher<T>, IDisposable
+{
+    List<Handler<T>> handlers;
+    public AbstractPublisher()
+    {
+        handlers = new List<Handler<T>>();
+    }
+
+    public async Task<Boolean> Publish(T? obj)
+    {
+        Console.WriteLine("[Encoded as an object]");
+        foreach (var handler in handlers)
+        {
+            obj = handler(obj);
+        } 
+        byte[] jsonUtf8Bytes =JsonSerializer.SerializeToUtf8Bytes<T?>(obj);
+        return await Publish(jsonUtf8Bytes);   
+    }
+
+    public async Task<Boolean> Publish(string? message)
+    {
+        Console.WriteLine("[Encoded as a string]");
+        var body = Encoding.UTF8.GetBytes(message ?? "FAILED");
+        return await Publish(body);   
+    }
+
+    private async Task<Boolean> Publish(byte[] message)
+    {
+        if (IsHealthyOrTryRecovery())
+        {
+            Task<Boolean> task = new Task<bool>( () => AsyncPublish(message) );
+            task.Start();
+
+            return await task;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected abstract bool AsyncPublish(byte[] body);
+
+    private Boolean IsHealthyOrTryRecovery()
+    {
+        if(IsHealthy())
+        {
+            return true;
+        }
+        // try to recover
+        return ResetConnection();
+    }
+    protected abstract Boolean IsHealthy();
+
+    protected abstract Boolean ResetConnection(bool dispose = true);
+
+
+    public abstract void Dispose();
+
+    public IPublisher<T> AddMsgHandler(Handler<T> handler)
+    {
+        handlers.Add(handler);
+        return this;
+    }
+}
---
+++ Subscriber/Subscriber.cs
@@ -0,0 +1,33 @@
+﻿namespace FastCSharp.SDK.Subscriber;
+
+delegate Boolean OnMessageCallback<T>(T? message);
+delegate T? Handler<T>(T? message);
+
+interface ISubscriber<T>
+{
+    /// <summary>
+    /// Registers the callback function as a message listenner.
+    /// Returns self to chain the addition of message handlers
+    /// </summary>
+    /// <param name="callback">the function to register as message listenner.</param>
+    /// <returns></returns>
+    public ISubscriber<T> Register(OnMessageCallback<T> callback);
+
+    /// <summary>
+    /// Registers handler function to handle message before deelivering to the callback
+    /// Returns self to chain the addition of message handlers
+    /// </summary>
+    /// <param name="handler"></param>
+    /// <returns></returns>
+    public ISubscriber<T> AddMsgHandler(Handler<T> handler);
+}
+
+interface IWorker<T>
+{
+    public Boolean OnMessageCallback(T? message);
+}
+
+abstract class AbstreactSubscriberFactory
+{
+    abstract public ISubscriber<T> NewSubscriber<T>();
+}
