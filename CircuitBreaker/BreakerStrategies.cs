f6711667fea4b8ca72aa0335c81a18911bdc4df7
João Pereira <pereira.joao@gmail.com>
2023-03-18 22:37:28 +0000

    Try 'fixing' sonarqube's Exception rules infringement.

--- CircuitBreaker/BreakerStrategies.cs
+++ CircuitBreaker/BreakerStrategies.cs
@@ -1,8 +1,17 @@
 namespace FastCSharp.Circuit_Breaker;
-class IncorrectInitializationException: Exception
+class IncorrectInitializationException : Exception
 {
+    public IncorrectInitializationException() : base()
+    {
+        // intentionally empty
+    }
     public IncorrectInitializationException(string message) : base(message)
     {
+        // intentionally empty
+    }
+    public IncorrectInitializationException(string message, Exception inner) : base(message, inner)
+    {
+        // intentionally empty
     }
 }
 
